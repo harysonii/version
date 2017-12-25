712dab3da67955865ff62656129d9d434fd08f6f
harysonii <haryson.onuoha@gmail.com>
2017-12-25 22:30:56 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(window).on("load",function (){
+               
+              // if( typeof window.localStorage.getItem("login") !== 'undefined' && window.localStorage.getItem("login") != 1) {
+              //      window.location.href="login.html";
+    
+              //  }else {
+    
+	           /*$.ajax({
+                
+                  type: 'get',
+                  url: 'http://gothere.000webhostapp.com/check_status.php',
+                   dataType: 'html',
+                  success: function (response) {
+                        
+                        if (response == "ok") {
+                            
+                        }else {
+                            window.location.href="login.html";
+                        }
+                        
+                    }
+                      
+                   
+               });  */
+                     preloaderFadeOutTime = 1500;
+        function hidePreloader() {
+            var preloader = $('.spinner-wrapper');
+            preloader.fadeOut(preloaderFadeOutTime);
+        }
+        hidePreloader();
+                
+               
+                });
